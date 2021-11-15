8d76845c34a9f57753f2edd2dc0a8a0707b175a4
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-15 16:20:19 +0100

    Js - Aggiunto evento: al click si chiude il menu

--- main.js
+++ main.js
@@ -2,13 +2,24 @@
 const burgerIcon = document.querySelector( "div.header-right > a" );
 
 // Seleziono il burger-menu
-const activeBurgerMenu = document.querySelector('.hamburger-menu');
+const burgerMenu = document.querySelector('.hamburger-menu');
 
 // Aggiunto evento - Al click sull'icona del burger si apre il menu
 burgerIcon.addEventListener('click',
 
     function() {
-        activeBurgerMenu.classList.add('active');
+        burgerMenu.classList.add('active');
+    }
+
+);
+
+const closeBurgerMenu = document.querySelector( ".close" );
+
+// Aggiunto evento - Al click sull'icona "close" si chiude il menu
+closeBurgerMenu.addEventListener('click',
+
+    function() {
+        burgerMenu.classList.remove('active');
     }
 
 );
